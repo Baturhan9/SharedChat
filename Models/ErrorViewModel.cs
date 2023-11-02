621783ffa471d5380abfce317a1507e0a2f6827f
Baturhan <baturxan-06.72@mail.ru>
2023-11-02 18:32:07 +0600

    start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SharedChatWebSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
